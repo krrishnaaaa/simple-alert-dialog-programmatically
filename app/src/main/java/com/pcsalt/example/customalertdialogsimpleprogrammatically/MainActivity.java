9ba4e22de114829bda84768b6863e5978f93996e
krrishnaaaa <krishna@pcsalt.com>
2020-05-01 14:23:27 +0530

    updated to androidx

--- app/src/main/java/com/pcsalt/example/customalertdialogsimpleprogrammatically/MainActivity.java
+++ app/src/main/java/com/pcsalt/example/customalertdialogsimpleprogrammatically/MainActivity.java
@@ -1,8 +1,8 @@
 package com.pcsalt.example.customalertdialogsimpleprogrammatically;
 
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.TypedValue;
 import android.widget.EditText;
 import android.widget.LinearLayout;
